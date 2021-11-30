e61d3473a15f714c268a77eecf613ffb23d92977
Divyanshu Varshney <51895957+divyanshu-95@users.noreply.github.com>
2021-11-30 10:59:38 +0530

    Add files via upload

---
+++ src/Emitter.ts
@@ -0,0 +1,24 @@
+import * as particles from "@pixi/particle-emitter";
+import { Container } from "pixi.js"
+const defaultOptions={
+"lifetime":{
+    "min":1.5,
+    "max":2.50
+},
+"frequency":0.005,
+"emitterLifetime":-1,
+"maxParticles":10000,
+"addAtBack":false,
+"pos":{
+    "x":0,
+    "y":0
+},
+
+};
+export class Emitter extends particles.Emitter{
+    constructor(parent:Container, config:any){
+        super(parent,{...defaultOptions, ...config});
+
+    }
+
+}
